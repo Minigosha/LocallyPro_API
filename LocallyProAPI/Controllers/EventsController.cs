b5a20ad841564e338c2c7afc7bdc4be487f8625f
Minigosha <margarita.lopez.romero@gmail.com>
2023-04-02 20:59:18 +0200

    in progress: fixing authorization & event controller

---
+++ LocallyProAPI/Controllers/EventsController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web.Resource;
+using Repositories.Context;
+using Repositories.Models;
+using System;
+
+namespace LocallyProAPI.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    //[Authorize]
+    //[AllowAnonymous]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EventsController(ApplicationDBContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet("")]
+        public async Task<IEnumerable<Event>> Index()
+        {
+            //var userID = _userManager.GetUserId(User);
+            //ApplicationUser user = await _userManager.GetUserAsync(User);
+            return await _context.Event.ToListAsync();
+        }
+        /*
+        [Route("[action]/city={City}&address={Address}&dateTimeStart={DateTimeStart}&dateTimeEnd={DateTimeEnd}")]
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<Event?> Create([Bind("Id,City,Address,DateTimeStart,DateTimeEnd")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return @event;
+            }
+            return null;
+        }
+        */
+        [HttpPost]
+        public async Task<ActionResult<Event>> Add(Event @event)
+        {
+            _context.Event.Add(@event);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("Events");
+            //return CreatedAtAction(nameof(Event), new { id = @event.Id }, @event);
+            return Ok(@event);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<Event>> DeleteEvent(int id)
+        {
+            try
+            {
+                var eventToDelete = await _context.Event.FindAsync(id);
+
+
+                if (eventToDelete == null)
+                {
+                    
+                    return NotFound($"Event with Id = {id} not found");
+                }
+
+                _context.Event.Remove(eventToDelete);
+                await _context.SaveChangesAsync();
+                return Ok(eventToDelete);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error deleting data");
+            }
+        }
+        /*
+        // GET: Events/Delete/
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Event == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }*/
+    }
+}
--- LocallyProAPI/Program.cs
+++ LocallyProAPI/Program.cs
@@ -2,11 +2,26 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Tokens;
 using Repositories.Context;
 using Repositories.Models;
 using System.Configuration;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -14,10 +29,57 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDBContext>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+
+/*
+ builder.Services.AddAuthentication("Default").AddJwtBearer("MyBearer")
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
                     .EnableTokenAcquisitionToCallDownstreamApi()
                         .AddInMemoryTokenCaches();
+*/
+builder.Services.AddAuthentication().AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+                        .EnableTokenAcquisitionToCallDownstreamApi()
+                        .AddInMemoryTokenCaches();
+//builder.Services.AddMvcCore().AddAuthorization();
+
+
+
+
+
+/*
+builder.Services
+    .AddAuthentication(builder.Configuration.GetRequiredSection("Authentication")
+        .GetValue<string>("DefaultScheme")!)
+        .AddJwtBearer("MyTest", options =>
+        {
+            options.Authority = builder.Configuration["AzureAd:Instance"] + builder.Configuration["AzureAd:TenantId"];
+            options.Audience = builder.Configuration["AzureAd:ClientId"];
+            options.TokenValidationParameters.ValidateIssuer = false;
+        })
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddDistributedTokenCaches();
+
+
+
+/*
+ * .AddJwtBearer(
+    "FusionAuth",
+    options => options.TokenValidationParameters.NameClaimType = "preferred_username")
+.AddJwtBearer("UserJwts");
+
+
+/*
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["AzureAd:Instance"] + builder.Configuration["AzureAd:TenantId"];
+    options.Audience = builder.Configuration["AzureAd:ClientId"];
+    options.TokenValidationParameters.ValidateIssuer = false;
+})
+.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+.EnableTokenAcquisitionToCallDownstreamApi()
+.AddDistributedTokenCaches();
+*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,10 +95,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
+
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
+/*app.UseRouting();*/
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
